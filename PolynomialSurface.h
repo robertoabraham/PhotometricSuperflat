73233cd94a8cd81cabd9c874424c7038770da2ce
Roberto Abraham <abraham@astro.utoronto.ca>
2014-02-05 18:35:58 -0500

    Added OS-independent gnuplot executable path; added PCL License.

--- PolynomialSurface.h
+++ PolynomialSurface.h
@@ -1,3 +1,47 @@
+/*******************************************************************************
+This file is part of the PhotometricSuperflat PixInsight module.
+
+Copyright (c) 2014, Roberto Abraham. All Rights Reserved.
+
+Redistribution and use in both source and binary forms, with or without
+modification, is permitted provided that the following conditions are met:
+
+1. All redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. All redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+3. Neither the names "PixInsight" and "Pleiades Astrophoto", nor the names of
+   their contributors, may be used to endorse or promote products derived from
+   this software without specific prior written permission. For written
+   permission, please contact info@pixinsight.com.
+
+4. All products derived from this software, in any form whatsoever, must
+   reproduce the following acknowledgment in the end-user documentation and/or
+   other materials provided with the product:
+
+   "This product is based on software from the PixInsight project, developed by
+    Pleiades Astrophoto and its contributors (http://pixinsight.com/)."
+
+   Alternatively, if that is where third-party acknowledgments normally appear,
+   this acknowledgment must be reproduced in the product itself.
+
+THIS SOFTWARE IS PROVIDED BY PLEIADES ASTROPHOTO AND ITS CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL PLEIADES ASTROPHOTO OR ITS CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, BUSINESS INTERRUPTION; PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; AND LOSS OF USE, DATA OR PROFITS) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*******************************************************************************/
+
+
 #ifndef __PolynomialSurface_h
 #define __PolynomialSurface_h
 
@@ -158,19 +202,9 @@ namespace pcl {
             gnuFile.Close();
 
             
-            // I need to determine the location of the PixInsight executable so I can 
-            // find the location of the gnuplot executable. I figured this might be in an
-            // environment variable but I can't seem to get at any environment variables. For now
-            // I will just hard-code the path to the executable
-            //
-            //StringList env = ExternalProcess().Environment();
-            //Console().WriteLn("Known environment variables:");
-            //for (StringList::const_iterator row = env.Begin(); row != env.End(); ++row) {
-            //    Console().WriteLn(*row);
-            //}
-            
             // Create the plot
-            ExternalProcess().ExecuteProgram("/Applications/PixInsight.app/Contents/MacOS/gnuplot " + gnuFilepath);
+            IsoString piBinDir( ::getenv( "PXI_BINDIR" ) );
+            ExternalProcess().ExecuteProgram(piBinDir + "/gnuplot " + gnuFilepath);
 
             // Display the plot window
             ImageWindow().Open(svgFilepath)[0].Show();
